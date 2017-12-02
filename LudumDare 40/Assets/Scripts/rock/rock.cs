92173a247a85b6a46a796dcd7723ff56d82f30df
arkasarius <roger.ordonez.brumos@gmail.com>
2017-12-02 17:17:39 +0100

    update de les 5 de la tarda

---
+++ LudumDare 40/Assets/Scripts/rock/rock.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rock : MonoBehaviour {
+    
+    
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+      
+    }
+
+    public void GoToPoint(Vector3 destination)
+    {
+       //implementam
+    }
+    private void OnDrawGizmos()
+    {
+        //Gizmos.DrawLine(transform.position, destination);
+    }
+}
