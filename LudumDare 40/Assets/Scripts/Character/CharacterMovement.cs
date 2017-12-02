13815402cc78e9791af461149b574c9af9ebce99
Josef212 <jcasanovasv@hotmail.com>
2017-12-02 19:45:23 +0100

    rock script

--- LudumDare 40/Assets/Scripts/Character/CharacterMovement.cs
+++ LudumDare 40/Assets/Scripts/Character/CharacterMovement.cs
@@ -47,8 +47,8 @@ public class CharacterMovement : MonoBehaviour {
     void SpawnRock()
     {
         GameObject rock = (GameObject)Instantiate(rockGameobject, skillspawnSkillsTrasnform.transform.position, Quaternion.identity);
-        Debug.Log(rock.transform.position);
-        rock.GetComponent<rock>().GoToPoint(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        //Debug.Log(rock.transform.position);
+        rock.GetComponent<Rock>().GoToPoint(Camera.main.ScreenToWorldPoint(Input.mousePosition), 20f); // TODO: Set the max throw distance as attribute and check the destination before call
     }
 
 
@@ -79,22 +79,22 @@ public class CharacterMovement : MonoBehaviour {
     {
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            Debug.Log("mouse R click at: " + Input.mousePosition);
+            //Debug.Log("mouse R click at: " + Input.mousePosition);
             
         }
         if (Input.GetKeyDown(KeyCode.Mouse1))
         {
-            Debug.Log("mouse L click at" + Input.mousePosition);
+            //Debug.Log("mouse L click at" + Input.mousePosition);
             ChangeEnergy(-1);
             SpawnRock();
         }
         if(Input.GetAxis("Mouse ScrollWheel") > 0f)
         {
-            Debug.Log("Mouse scroll UP");
+            //Debug.Log("Mouse scroll UP");
             ChangeSkillFocus(1);
         }else if (Input.GetAxis("Mouse ScrollWheel") < 0f)
         {
-            Debug.Log("Mouse scroll DOWN");
+            //Debug.Log("Mouse scroll DOWN");
             ChangeSkillFocus(-1);
         }
     }
@@ -106,7 +106,7 @@ public class CharacterMovement : MonoBehaviour {
     {
         skillFocus += amount;
         skillFocus=Mathf.Clamp(skillFocus,0,skills.Length-1);
-        Debug.Log("skill selected: "+skillFocus);
+        //Debug.Log("skill selected: "+skillFocus);
         foreach(Image skill in skills)
         {
             skill.color = Color.white;
--- LudumDare 40/Assets/Scripts/rock/rock.cs
+++ LudumDare 40/Assets/Scripts/rock/rock.cs
@@ -2,26 +2,89 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class rock : MonoBehaviour {
+public class Rock : MonoBehaviour
+{
+	private Vector3 destination;
+	private Vector2 velocity;
+
+	public float rockSpeed = 3f;
+	private float speed;
+
+	public float noiseDefaultRadius = 2f;
+	private float noiseRadius;
+
+	public LayerMask noiseDetectorsLayer;
+
+	public GameObject area;
+
+	private bool mustEndMovement = false;
+
+	private Rigidbody2D rb;
     
-    
-	// Use this for initialization
-	void Start () {
-        
+	//-------------------------------------------------
+
+	void Awake()
+	{
+		rb = GetComponent<Rigidbody2D>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
 
-      
-    }
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		mustEndMovement = true;
+	}
 
-    public void GoToPoint(Vector3 destination)
-    {
-       //implementam
-    }
-    private void OnDrawGizmos()
-    {
-        //Gizmos.DrawLine(transform.position, destination);
-    }
+	//-------------------------------------------------
+
+	public void GoToPoint(Vector3 _destination, float maxPlayerThrowDistance)
+	{
+		destination = _destination;
+		destination.z = 0;
+
+		// Will set speed according to the distance.
+		speed = rockSpeed; //(Vector3.Distance(transform.position, _destination) / maxPlayerThrowDistance) * rockSpeed;
+
+		Vector2 dir = (destination - transform.position);
+		dir.Normalize();
+		velocity = dir * speed;
+
+		StartCoroutine(MoveRock());
+	}
+
+	IEnumerator MoveRock()
+	{
+		//Debug.Log("Movement corutine started.");
+		while (Vector3.Distance(transform.position, destination) >= 0.1 && !mustEndMovement) //Let a bit of threshold
+		{
+			rb.position += velocity * Time.deltaTime;
+
+			// TODO: Animatios
+
+			yield return null;
+		}
+
+		if (mustEndMovement)
+		{
+			// Bounce??
+		}
+
+		ProduceNoise();
+	}
+
+	void ProduceNoise()
+	{
+		area.SetActive(true);
+
+		// Collect all listeners could listen to the rock.
+
+		Collider2D[] targets = Physics2D.OverlapCircleAll(rb.position, noiseDefaultRadius, noiseDetectorsLayer);
+		foreach (Collider2D target in targets)
+		{
+			target.BroadcastMessage("OnNoise",  target.transform.position); //TODO: 
+		}
+
+
+		//Debug.Log("Rock arrived");
+		// TODO: Can play an animation to decrease the alpha and destroy it after that
+		Destroy(gameObject, 1f);
+	}
 }
